1b480dd448a7e050cad909676278c5d5e0cdd92b
Marie <marie@TT-no-MacBook-Air-3.local>
2024-05-27 11:27:21 +0900

    first commit

---
+++ swiftui_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_trainingApp.swift
+//  swiftui-training
+//
+//  Created by Marie on 2024/05/27.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
